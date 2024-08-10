b7105f032ae181bac8eadc00ee76cc1d6450b181
Ali İhsan Şenel <aliihsansenel0@gmail.com>
2024-08-10 14:41:36 +0300

    character visibility

--- src/entities/Character.tsx
+++ src/entities/Character.tsx
@@ -1,15 +1,19 @@
 
 import { useGLTF, useAnimations } from '@react-three/drei';
 import CharacterAnimController from '../controllers/anim/CharacterAnimController';
+import { useContext } from 'react';
+import { CameraTargetContext } from '../controllers/CameraController';
 
 const Character = () => {
   const { scene, animations } = useGLTF('/models/robot.glb');
   const { actions } = useAnimations(animations, scene);
 
+  const ctc = useContext(CameraTargetContext);
+
   return (
     <>
-      <CharacterAnimController actions={actions }/>
-      <primitive object={scene} scale={[0.55, 0.55, 0.55]}/>
+      <CharacterAnimController actions={actions } />
+      <primitive object={scene} scale={[0.55, 0.55, 0.55]} visible={!ctc?.isCameraToggled}/>
     </>
   );
 }
